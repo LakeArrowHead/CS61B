72035e30759922f9bd4fcadb23bccd17779454a2
Wang <menwang@deloitte.com>
2018-01-12 14:31:30 -0600

    Completed lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		int t = 0;
+		while (x < 10) {
+			t = t + x;
+			System.out.print(t + " ");
+			x = x + 1;
+
+		}
+	}
+}
